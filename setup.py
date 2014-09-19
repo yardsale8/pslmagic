bca5f281aded6db1842515e90e8de5dc55ee452d
Todd Iverson <tiverson@smumn.edu>
2014-09-19 14:47:58 -0500

    Added the setup file for pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pslmagic',
+            version='0.1',
+            description='IPython magic for ParselTongue',
+            url='http://github.com/yardsale8/pslmagic',
+            author='Todd Iverson',
+            author_email='tiverson@smumn.edu',
+            license='MIT',
+            packages=['pslmagic'],
+            zip_safe=False)
