10cf6799b42df264f9318bcabc6e451fab34fa37
Todd Iverson <tiverson@smumn.edu>
2014-09-21 10:01:52 -0500

    added a github like to the version

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='pslmagic',
             author='Todd Iverson',
             author_email='tiverson@smumn.edu',
             license='MIT',
+            download_url = 'https://github.com/yardsale8/pslmagic/tarball/0.1',
             packages=['pslmagic'],
             zip_safe=False)
